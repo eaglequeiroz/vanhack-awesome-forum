26c7ed1a272a3a30febc00e76345d789b97830b9
Igor Queiroz <igorcenourox@gmail.com>
2017-12-23 15:54:42 -0300

    First commit...

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,48 @@
+class TopicsController < ApplicationController
+  before_action :set_topic, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def edit
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+      if @topic.save
+        redirect_to @topic, notice: 'Topic was successfully created.'
+      else
+        render :new
+    end
+  end
+
+  def update
+      if @topic.update(topic_params)
+        redirect_to @topic, notice: 'Topic was successfully updated.'
+      else
+        render :edit
+    end
+  end
+
+  def destroy
+    @topic.destroy
+  end
+
+  private
+    def set_topic
+      @topic = Topic.find(params[:id])
+    end
+
+    def topic_params
+      params.require(:topic).permit(:title, :content)
+    end
+end
---
+++ app/views/topics/_topic.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! topic, :id, :title, :content, :created_at, :updated_at
+json.url topic_url(topic, format: :json)
