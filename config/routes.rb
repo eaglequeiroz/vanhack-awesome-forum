9db1fcdfc512b9c2615d90e744c90ad399303235
Igor Queiroz <igorcenourox@gmail.com>
2017-12-25 18:34:54 -0300

    Fully Functional Login and Logout system

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root to: 'topics#index'
+
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :topics
   resources :users
