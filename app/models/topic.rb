260f941d3d99af0333888782a6f37fc8dfe7cba1
Igor Queiroz <igorcenourox@gmail.com>
2017-12-28 12:28:20 -0300

    Removing the maximun character restriction of topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 15, maximum: 150 }
-  validates :content, presence: true, length: { minimum: 20, maximum: 2000 }
+  validates :title, presence: true, length: { minimum: 10, maximum: 150 }
+  validates :content, presence: true, length: { minimum: 20 }
   belongs_to :user
   has_many :comments, dependent: :destroy
   default_scope -> { order(updated_at: :desc, created_at: :desc) }
